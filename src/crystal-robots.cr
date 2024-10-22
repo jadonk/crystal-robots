cda38dc25f29626bcf5ba59c2e613f6597dd4c7c
Jason Kridner <jkridner@beagleboard.org>
2024-10-22 06:40:09 -0400

    wip: failing

--- src/crystal-robots.cr
+++ src/crystal-robots.cr
@@ -6,7 +6,7 @@ module CrystalRobots
 
   class Emitter
     def unsignedLEB128(n)
-      buffer = Bytes.new()
+      buffer = Bytes[]
       loop do
         byte = n & 0xff
         n = n >> 7
@@ -26,7 +26,7 @@ module CrystalRobots
     end
 
     # https://webassembly.github.io/spec/core/binary/modules.html#sections
-    enum Section
+    enum Section : UInt8
       Custom
       Type
       Import
@@ -41,20 +41,20 @@ module CrystalRobots
     end
 
     # https://webassembly.github.io/spec/core/binary/types.html
-    enum Valtype
+    enum Valtype : UInt8
       I32 = 0x7f
       F32 = 0x7d
     end
 
     # https://webassembly.github.io/spec/core/binary/instructions.html
-    enum Opcodes
+    enum Opcodes : UInt8
       End = 0x0b
       Get_local = 0x20
       F32_add = 0x92
     end
 
     # http://webassembly.github.io/spec/core/binary/modules.html#export-section
-    enum ExportType
+    enum ExportType : UInt8
       Func = 0x00
       Table = 0x01
       Mem = 0x02
@@ -64,7 +64,7 @@ module CrystalRobots
     # http://webassembly.github.io/spec/core/binary/types.html#function-types
     FunctionType = Bytes[0x60]
 
-    EmptyArray = 0
+    EmptyArray = Bytes[0]
 
     # https://webassembly.github.io/spec/core/binary/modules.html#binary-module
     MagicModuleHeader = Bytes[0,'a'.ord,'s'.ord,'m'.ord]
@@ -86,32 +86,31 @@ module CrystalRobots
     # WebAssembly only supports single return values
     def addFunctionType
       FunctionType +
-      encodeVector([Valtype.F32, Valtype.F32]) +
-      encodeVector([Valtype.F32])
+      encodeVector([Valtype::F32.value, Valtype::F32.value]) +
+      encodeVector([Valtype::F32.value])
     end
 
     # the type section is a vector of function types
     def typeSection
-      Section.Type +
-      encodeVector([addFunctionType])
+      createSection(Section::Type.value, encodeVector([addFunctionType]))
     end
 
     # the function section is a vector of type indices that indicate the type of each function
     # in the code section
     def funcSection
-      createSection(Section.Func, encodeVector([0x00])) # type index
+      createSection(Section::Func.value, encodeVector([0x00])) # type index
     end
 
     # the export section is a vector of exported functions
     def exportSection
-      createSection(Section.Export, encodeVector(
-        [encodeString("run"), ExportType.Func, 0x00] # function index
+      createSection(Section::Export.value, encodeVector(
+        [encodeString("run"), ExportType::Func.value, 0x00] # function index
       ))
     end
 
     # the code section contains vectors of functions
     def code
-      [
+      Bytes[
         Opcodes.Get_local,
         unsignedLEB128(0),
         Opcodes.Get_local,
@@ -120,10 +119,24 @@ module CrystalRobots
       ]
     end
 
+    def functionBody
+      encodeVector(EmptyArray + code + Bytes[Opcodes.End])
+    end
+
+    def codeSection
+      Bytes[
+        Section.Code,
+        encodeVector(functionBody)
+      ]
+    end
+
     def emitter
       MagicModuleHeader +
       ModuleVersion +
-      0
+      typeSection +
+      funcSection +
+      exportSection +
+      codeSection
     end
   end
 end
