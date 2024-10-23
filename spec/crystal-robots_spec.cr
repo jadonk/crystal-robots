8a62e0371597c50e8364fff302778e430e08d389
Jason Kridner <jkridner@beagleboard.org>
2024-10-23 08:34:12 -0400

    test object is Compiler

--- spec/crystal-robots_spec.cr
+++ spec/crystal-robots_spec.cr
@@ -2,14 +2,14 @@ require "spec"
 require "../src/crystal-robots.cr"
 
 describe CrystalRobots do
-  describe "Emitter" do
+  describe "Compiler" do
     it "can be instantiated" do
-      c = CrystalRobots::Emitter.new
+      c = CrystalRobots::Compiler.new
       c.should_not eq nil
     end
 
     it "has an emitter" do
-      c = CrystalRobots::Emitter.new
+      c = CrystalRobots::Compiler.new
       # https://webassembly.github.io/wabt/demo/wat2wasm/
       c.emitter.should eq Bytes[
         0,0x61,0x73,0x6d,                  # WASM_BINARY_MAGIC
