a0229e5f083333a4d98aef71ee34d9510d4d271c
Jason Kridner <jkridner@beagleboard.org>
2024-10-25 12:52:41 -0400

    run 'crystal tool format' !

--- spec/crystal-robots_spec.cr
+++ spec/crystal-robots_spec.cr
@@ -26,12 +26,12 @@ describe CrystalRobots do
       c = CrystalRobots::Compiler.new
       # https://webassembly.github.io/wabt/demo/wat2wasm/
       c.emitter.should eq Bytes[
-        0,0x61,0x73,0x6d,                  # WASM_BINARY_MAGIC
-        1,0,0,0,                           # WASM_BINARY_VERSION
-        1,7,1,0x60,2,0x7d,0x7d,1,0x7d,     # Section "Type"
-        3,2,1,0,                           # Section "Function"
-        7,7,1,3,0x72,0x75,0x6e,0,0,        # Section "Export"
-        10,9,1,7,0,0x20,0,0x20,1,0x92,0xb  # Section "Code"
+        0, 0x61, 0x73, 0x6d,                        # WASM_BINARY_MAGIC
+        1, 0, 0, 0,                                 # WASM_BINARY_VERSION
+        1, 7, 1, 0x60, 2, 0x7d, 0x7d, 1, 0x7d,      # Section "Type"
+        3, 2, 1, 0,                                 # Section "Function"
+        7, 7, 1, 3, 0x72, 0x75, 0x6e, 0, 0,         # Section "Export"
+        10, 9, 1, 7, 0, 0x20, 0, 0x20, 1, 0x92, 0xb # Section "Code"
       ]
     end
   end
--- src/rabbit.cr
+++ src/rabbit.cr
@@ -8,19 +8,19 @@ rabbit = Robot.new("Rabbit")
 
 rabbit.main do |r|
   while true
-    go(r,rand(1000),rand(1000))   # go somewhere on the field
+    go(r, rand(1000), rand(1000)) # go somewhere on the field
   end
 end
 
 # go - go to the point specified
-def go(r,dest_x,dest_y)
-  course = plot_course(r,dest_x,dest_y)
-  r.drive(course,25)
-  while distance(r.loc_x(),r.loc_y(),dest_x,dest_y) > 50
+def go(r, dest_x, dest_y)
+  course = plot_course(r, dest_x, dest_y)
+  r.drive(course, 25)
+  while distance(r.loc_x, r.loc_y, dest_x, dest_y) > 50
     # continue while distance is greater than 50
   end
-  r.drive(course,0)
-  while r.speed() > 0
+  r.drive(course, 0)
+  while r.speed > 0
     # continue while speed is greater than 0
   end
 end
@@ -29,14 +29,14 @@ end
 def distance(x1, y1, x2, y2)
   x = x1 - x2
   y = y1 - y2
-  sqrt((x*x)+(y*y))
+  sqrt((x*x) + (y*y))
 end
 
 # plot_course - figure out which heading to go
 def plot_course(r, xx, yy)
   scale = 100000 # scale for trig functions
-  curx = r.loc_x()
-  cury = r.loc_y()
+  curx = r.loc_x
+  cury = r.loc_y
   x = curx - xx
   y = cury - yy
   if x == 0
