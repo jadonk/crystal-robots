6a7863c759deb215b1dab881ad8e8e41361141a6
Jason Kridner <jkridner@beagleboard.org>
2024-10-29 09:01:02 -0400

    wip: broken

--- lib/crystal_robots.cr
+++ lib/crystal_robots.cr
@@ -71,7 +71,7 @@ module CrystalRobots
       end
     end
 
-    private def debug(s)
+    def debug(s)
       @calls += 1
       puts "#{@calls}: #{s}"
       if @calls >= MAX_CALLS
@@ -144,58 +144,6 @@ module CrystalRobots
       @rang = 0      # range of missile
       @curr_dist = 0 # current distance from origin * 100
     end
-  end
-
-  def rand(limit)
-    r = Random.rand(limit)
-    debug "rand #{limit} -> #{r}"
-    r
-  end
-
-  def sqrt(number)
-    r = Math.isqrt(number.abs)
-    debug "sqrt #{number} -> #{r}"
-    r
-  end
-
-  def sin(degree)
-    r = f2i(Math.sin(d2r(degree)))
-    debug "sin #{degree} -> #{r}"
-    r
-  end
-
-  def cos(degree)
-    r = f2i(Math.cos(d2r(degree)))
-    debug "cos #{degree} -> #{r}"
-    r
-  end
-
-  def tan(degree)
-    r = f2i(Math.tan(d2r(degree)))
-    debug "tan #{degree} -> #{r}"
-    r
-  end
-
-  def atan(ratio)
-    r = r2d(Math.atan(i2f(ratio)))
-    debug "tan #{ratio} -> #{r}"
-    r
-  end
-
-  private def d2r(degree)
-    degree * Math::PI / 180
-  end
-
-  private def r2d(radian)
-    (radian * 180 / Math::PI).to_i32
-  end
-
-  private def f2i(x)
-    (x*100000).to_i32
-  end
-
-  private def i2f(n)
-    (n/100000).to_f32
   end
 
   # Crystal Robots compiler
@@ -455,4 +403,56 @@ module CrystalRobots
 
   class Battlefield
   end
+end
+
+def rand(limit)
+  r = Random.rand(limit)
+  CrystalRobots::debug "rand #{limit} -> #{r}"
+  r
+end
+
+def sqrt(number)
+  r = Math.isqrt(number.abs)
+  CrystalRobots::debug "sqrt #{number} -> #{r}"
+  r
+end
+
+def sin(degree)
+  r = f2i(Math.sin(d2r(degree)))
+  CrystalRobots::debug "sin #{degree} -> #{r}"
+  r
+end
+
+def cos(degree)
+  r = f2i(Math.cos(d2r(degree)))
+  CrystalRobots::debug "cos #{degree} -> #{r}"
+  r
+end
+
+def tan(degree)
+  r = f2i(Math.tan(d2r(degree)))
+  CrystalRobots::debug "tan #{degree} -> #{r}"
+  r
+end
+
+def atan(ratio)
+  r = r2d(Math.atan(i2f(ratio)))
+  CrystalRobots::debug "tan #{ratio} -> #{r}"
+  r
+end
+
+private def d2r(degree)
+  degree * Math::PI / 180
+end
+
+private def r2d(radian)
+  (radian * 180 / Math::PI).to_i32
+end
+
+private def f2i(x)
+  (x*100000).to_i32
+end
+
+private def i2f(n)
+  (n/100000).to_f32
 end
