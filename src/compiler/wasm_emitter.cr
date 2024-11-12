5cd38e2d2e3886c3f5e108322f47a0a762c286ce
Jason Kridner <jkridner@beagleboard.org>
2024-11-12 00:31:10 -0500

    wip: broken

--- src/compiler/wasm_emitter.cr
+++ src/compiler/wasm_emitter.cr
@@ -2,8 +2,11 @@
 
 module CrystalRobots::Compiler
   class WASM_Emitter
+    @code : Bytes
+
     def initialize(ast : Program)
       @program = ast
+      @code = Bytes[]
     end
 
     # https://en.wikipedia.org/wiki/LEB128
@@ -15,10 +18,31 @@ module CrystalRobots::Compiler
         if n != 0
           byte |= 0x80
         end
-        buffer = buffer + Bytes[byte]
+        buffer += Bytes[byte]
         if n == 0
           break
         end
+      end
+      buffer
+    end
+
+    def signedLEB128(n : Int32) : Bytes
+      buffer = Bytes[]
+      more = true
+      isNegative = n.negative?
+      bitCount = n.bit_length
+      while more
+        byte = n & 0x7f
+        n = n >> 7
+        if isNegative
+          n = n | -(1 << (bitCount - 8))
+        end
+        if (n == 0 && (byte & 0x40) == 0) || (n == -1 && (byte & 0x40) != 0x40)
+          more = false
+        else
+          byte = byte | 0x80
+        end
+        buffer += Bytes[byte]
       end
       buffer
     end
@@ -68,7 +92,9 @@ module CrystalRobots::Compiler
       End       = 0x0b
       Call      = 0x10
       Get_local = 0x20
+      I32_const = 0x41
       F32_const = 0x43
+      I32_add   = 0x6a
       F32_add   = 0x92
     end
 
@@ -94,17 +120,23 @@ module CrystalRobots::Compiler
 
     # Function types are vectors of parameters and return types. Currently
     # WebAssembly only supports single return values
-    def addFunctionType
+    def int32Int32Type
       Bytes[FunctionType] +
-        encodeVector(Bytes[Valtype::F32.value, Valtype::F32.value]) +
-        encodeVector(Bytes[Valtype::F32.value])
+        encodeVector(Bytes[Valtype::I32.value]) +
+        encodeVector(Bytes[Valtype::I32.value])
+    end
+
+    def voidVoidType
+      Bytes[FunctionType] +
+        Bytes[0] +
+        Bytes[0]
     end
 
     # the type section is a vector of function types
     def typeSection
       createSection(Section::Type,
-        Bytes[1] + # number of types
-        addFunctionType()
+        encodeVector(voidVoidType) +
+        encodeVector(int32Int32Type)
       )
     end
 
@@ -117,56 +149,48 @@ module CrystalRobots::Compiler
       )
     end
 
+    def importSection
+      createSection(Section::Import,
+        encodeVector(
+          encodeString("env") +
+          encodeString("puts") +
+          Bytes[ExportType::Func.value] +
+          Bytes[0x01]
+        )
+      )
+    end
+
     # the export section is a vector of exported functions
     def exportSection
       createSection(Section::Export,
         Bytes[1] + # number of exports
         encodeString("run") +
         Bytes[ExportType::Func.value] + # export type
-        Bytes[0x00]                     # function index
+        Bytes[0x01]                     # function index
       )
     end
 
-    enum NodeType
-      Program
-      Statement
-      NumberLiteral
-      Identifier
-      PrintStatement
-    end
-
-    struct Node
-      property type
-    end
-
-    struct ExpressionNode
-      property value
-      forward_missing_to @node
-
-      def initilize(@node : Node, @value : Float32)
-        @node.type = NodeType::NumberLiteral
-      end
-    end
-
-    def emitExpression(node : ExpressionNode)
-      case node.type
-      when NodeType::NumberLiteral
-        @code << Bytes[Opcodes::F32_const.local]
-        @code << ieee754(node.value)
-      end
-    end
-
     def codeFromAst(ast : Program)
-      # code = Bytes.new
-      # code << emitExpression
-      Bytes[0] + # number of locals
-        Bytes[Opcodes::Get_local.value] +
-        Bytes[0] + # index 0
-        Bytes[Opcodes::Get_local.value] +
-        Bytes[1] + # index 1
-        Bytes[Opcodes::F32_add.value] +
-        Bytes[Opcodes::End.value]
-      # code
+      @code = Bytes[]
+      ast.ast.each_index(start: -1, count: ast.ast.size) do |i|
+        ast.ast[i].each_index do |j|
+          case ast.ast[i][j].type
+          when Tokenizer::Type::OneArgStatement
+            a = ast.ast[i][j].index[1]
+            t = ast.ast[i - 1][a]
+            case t.type
+            when Tokenizer::Type::Number
+              @code += Bytes[Opcodes::I32_const.value]
+              @code += signedLEB128(t.value.to_i32)
+            else
+              raise "Unsupported argument type"
+            end
+            @code += Bytes[Opcodes::Call.value]
+            @code += unsignedLEB128(0)
+          end
+        end
+      end
+      @code
     end
 
     # the code section contains vectors of functions
@@ -174,6 +198,7 @@ module CrystalRobots::Compiler
       createSection(Section::Code,
         Bytes[1] + # number of functions
         encodeVector(codeFromAst(ast))
+        Bytes[Opcodes::End]
       )
     end
 
@@ -183,6 +208,7 @@ module CrystalRobots::Compiler
       MagicModuleHeader +
         ModuleVersion +
         typeSection +
+        importSection +
         funcSection +
         exportSection +
         codeSection(ast)
