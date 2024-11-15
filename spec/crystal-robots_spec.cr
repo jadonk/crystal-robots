734ba6790a35bafa708713e7ca661800f9851a21
Jason Kridner <jkridner@beagleboard.org>
2024-11-14 19:57:06 -0500

    wat formatting

--- spec/crystal-robots_spec.cr
+++ spec/crystal-robots_spec.cr
@@ -81,71 +81,60 @@ describe CrystalRobots do
       f = CrystalRobots::Compiler.compile_to_wasm(" puts 42")
       # https://webassembly.github.io/wabt/demo/wat2wasm/
       f.should eq Bytes[
-        0, 0x61, 0x73, 0x6d,                                     # WASM_BINARY_MAGIC
-        1, 0, 0, 0,                                              # WASM_BINARY_VERSION
-        0x01,                                                    # section "Type" - section code
-        19,                                                      # section size
-        0x04,                                                    # num types
-        0x60,                                                    # func type 0 - func
-        0,                                                       # num params
-        0,                                                       # num results
-        0x60,                                                    # func type 1 - func
-        0,                                                       # num params
-        1,                                                       # num results
-        0x7f,                                                    # i32
-        0x60,                                                    # func type 2 - func
-        1,                                                       # num params
-        0x7f,                                                    # i32
-        1,                                                       # num results
-        0x7f,                                                    # i32
-        0x60,                                                    # func type 3 - func
-        2,                                                       # num params
-        0x7f,                                                    # i32
-        0x7f,                                                    # i32
-        1,                                                       # num results
-        0x7f,                                                    # i32
-        2, 12, 1, 3, 101, 110, 118, 4, 112, 117, 116, 115, 0, 2, # Section "Import"
-        3, 2, 1, 1,                                              # Section "Function"
-        7, 7, 1, 3, 0x72, 0x75, 0x6e, 0, 1,                      # Section "Export"
-        10, 8, 1, 6, 0, 65, 42, 16, 0, 0xb                       # Section "Code"
+        0, 0x61, 0x73, 0x6d,    # WASM_BINARY_MAGIC
+        1, 0, 0, 0,             # WASM_BINARY_VERSION
+        0x01,                   # section "Type" - section code
+        19,                     # section size
+        0x04,                   # num types
+        0x60,                   # func type 0 - func
+        0,                      # num params
+        0,                      # num results
+        0x60,                   # func type 1 - func
+        0,                      # num params
+        1,                      # num results
+        0x7f,                   # i32
+        0x60,                   # func type 2 - func
+        1,                      # num params
+        0x7f,                   # i32
+        1,                      # num results
+        0x7f,                   # i32
+        0x60,                   # func type 3 - func
+        2,                      # num params
+        0x7f,                   # i32
+        0x7f,                   # i32
+        1,                      # num results
+        0x7f,                   # i32
+        2,                      # section "Import" - section code
+        12,                     # section size
+        1,                      # num imports
+        3,                      # import header 0 - string length
+        0x65, 0x6e, 0x76,       # import module name - "env"
+        4,                      # string length
+        0x70, 0x75, 0x74, 0x73, # import field name - "puts"
+        0,                      # import kind
+        2,                      # import signature index
+        3,                      # section "Function" - section code
+        2,                      # section size
+        1,                      # num functions
+        1,                      # function 0 signature index
+        7,                      # section "Export" - section code
+        7,                      # section size
+        1,                      # num exports
+        3,                      # string length
+        0x72, 0x75, 0x6e,       # export name - "run"
+        0,                      # export kind
+        1,                      # export func index
+        10,                     # section "Code" - section code
+        8,                      # section size
+        1,                      # num functions
+        6,                      # function body 0 - func body size
+        0,                      # local decl count
+        0x41,                   # i32.const
+        42,                     # i32 literal
+        0x10,                   # call
+        0,                      # function index
+        0x0b,                   # end
       ]
-      # ; section "Import" (2)
-      # 000001d: 02                                        ; section code
-      # 000001e: 00                                        ; section size (guess)
-      # 000001f: 01                                        ; num imports
-      # ; import header 0
-      # 0000020: 03                                        ; string length
-      # 0000021: 656e 76                                    ; import module name - "env"
-      # 0000024: 04                                        ; string length
-      # 0000025: 7075 7473                                  ; import field name - "puts"
-      # 0000029: 00                                        ; import kind
-      # 000002a: 02                                        ; import signature index
-      # 000001e: 0c                                        ; FIXUP section size
-      # ; section "Function" (3)
-      # 000002b: 03                                        ; section code
-      # 000002c: 02                                        ; section size
-      # 000002d: 01                                        ; num functions
-      # 000002e: 01                                        ; function 0 signature index
-      # ; section "Export" (7)
-      # 000002f: 07                                        ; section code
-      # 0000030: 07                                        ; section size
-      # 0000031: 01                                        ; num exports
-      # 0000032: 03                                        ; string length
-      # 0000033: 7275 6e                                   ; export name - "run"
-      # 0000036: 00                                        ; export kind
-      # 0000037: 01                                        ; export func index
-      # ; section "Code" (10)
-      # 0000038: 0a                                        ; section code
-      # 0000039: 08                                        ; section size
-      # 000003a: 01                                        ; num functions
-      # ; function body 0
-      # 000003b: 06                                        ; FIXUP func body size
-      # 000003c: 00                                        ; local decl count
-      # 000003d: 41                                        ; i32.const
-      # 000003e: 2a                                        ; i32 literal
-      # 000003f: 10                                        ; call
-      # 0000040: 00                                        ; function index
-      # 0000041: 0b                                        ; end
     end
 
     it "test function should load the emited WASM" do
